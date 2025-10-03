38f524d7c4fb6241896e2706f30b9b078ed17105
Lucy <luu.anzoateguy@gmail.com>
2025-10-03 18:09:36 -0300

    Creo la clase para los objetivos y añado sus métodos

--- dohabits.py
+++ dohabits.py
@@ -1,7 +1,7 @@
 # Lógica principal de mi app
 
 class Usuario:
-    def __init__(self, nombre, ):
+    def __init__(self, nombre):
         self.nombre = nombre
         self.objetivos = []
 
@@ -14,3 +14,16 @@ class Usuario:
     def eliminar_objetivo(self, nombre_objetivo):
         self.objetivos = [o for o in self.objetivos if o.nombre != nombre_objetivo]
 
+class Objetivo:
+    def __init__(self, nombre):
+        self.nombre = nombre
+        self.tareas = []
+
+    def agregar_tarea(self, tarea):
+        self.tareas.append(tarea)
+
+    def progreso(self):
+        if not self.tareas:
+            return 0
+        tareas_completadas = sum(tarea.completadas for tarea in self.tareas)
+        return round((tareas_completadas / len(self.tareas) * 100, 2))
