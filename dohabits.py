ab3c2d67eb14e67066b4688889361715e6816cfc
Lucy <luu.anzoateguy@gmail.com>
2025-10-06 06:52:50 -0300

    Agrego clase de Tarea y soluciono pequeños errores

--- dohabits.py
+++ dohabits.py
@@ -19,16 +19,30 @@ class Objetivo:
         self.nombre = nombre
         self.tareas = []
 
-    def agregar_tarea(self, tarea):
-        self.tareas.append(tarea)
+    def agregar_tarea(self, nombre_tarea):
+        nueva_tarea = Tarea(nombre_tarea)
+        self.tareas.append(nueva_tarea)
 
     def progreso(self):
         if not self.tareas:
             return 0
-        tareas_completadas = sum(tarea.completadas for tarea in self.tareas)
+        tareas_completadas = sum(tarea.completada for tarea in self.tareas)
         return round((tareas_completadas / len(self.tareas) * 100, 2))
     
     def __str__(self):
-        return f"{self.nombre - self.progreso()}% completado"
-    
-    
+        return f"{self.nombre} - {self.progreso()}% completado"
+
+class Tarea:
+    def __init__(self, nombre):
+        self.nombre = nombre
+        self.completada = False
+
+    def marcar_completada(self):
+        self.completada = True
+
+    def desmarcar_completada(self):
+        self.completada = False
+
+    def __str__(self):
+        estado = "✔" if self.completada else "✘"
+        return f"{self.nombre} [{estado}]"
