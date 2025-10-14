02c16694425a49479f3a33badccb39cfae8e8030
Lucy <luu.anzoateguy@gmail.com>
2025-10-14 12:56:27 -0300

    Endpoints de html template y usuario

--- dohabits_api.py
+++ dohabits_api.py
@@ -1,2 +1,20 @@
 from flask import Flask, request, jsonify, render_template
 from dohabits import Usuario, Objetivo, Tarea
+
+app = Flask(__name__)
+
+usuarios = {} # Diccionario global de usuarios
+
+@app.route("/")
+def home():
+    return render_template("")
+
+@app.route("/usuarios/iniciar", methods=["POST"])
+def iniciar_usuario():
+    data = request.json
+    nombre = data["nombre"]
+
+    if nombre not in usuarios:
+        usuarios[nombre] = Usuario(nombre)
+
+    return jsonify({"mensaje": f"Usuario {nombre} iniciado"})
