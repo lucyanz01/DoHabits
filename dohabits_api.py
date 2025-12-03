e23f83a51ba1a8a2477e4feeda5efa713c353c3d
Lucy <luu.anzoateguy@gmail.com>
2025-12-03 13:01:48 -0300

    Agrego endpoint de usuarios

--- dohabits_api.py
+++ dohabits_api.py
@@ -12,3 +12,24 @@ load_dotenv()
 engine = create_engine(os.getenv("DATABASE_URL"))
 Session = sessionmaker(bind=engine)
 session = Session()
+
+@app.route("/")
+def home():
+    return "Funcionando"
+
+@app.route("/usuarios", methods = ["POST"])
+def crear_usuario():
+    data = request.get_json()
+
+    if "username" not in data or "password" not in data:
+        return jsonify({"error": "Faltan nombre de usuario o contraseña"}), 400
+    
+    nuevo_usuario = Usuario(
+        username = data["username"],
+        password = data["password"]
+    )
+
+    session.add(nuevo_usuario)
+    session.commit()
+    
+    return jsonify({"mensaje": "Usuario creado", "id": nuevo_usuario.id}), 201
