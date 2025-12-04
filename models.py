7c14e447e5d15f14a8feb1e821f6ddb65280e9fe
Lucy <luu.anzoateguy@gmail.com>
2025-12-04 12:06:49 -0300

    Agregando funciones a models.py

--- models.py
+++ models.py
@@ -22,6 +22,12 @@ class Objetivo(Base):
     usuario = relationship("Usuario", back_populates="objetivos")
     tareas = relationship("Tarea", back_populates="objetivo")
 
+    def progreso(self):
+        if not self.tareas:
+            return 0
+        completadas = sum(1 for t in self.tareas if t.completada)
+        return round(completadas / len(self.tareas) * 100, 2)
+    
 class Tarea(Base):
     __tablename__ = "tareas"
 
@@ -31,3 +37,9 @@ class Tarea(Base):
     objetivo_id = Column(Integer, ForeignKey("objetivos.id"), nullable=False)
 
     objetivo = relationship("Objetivo", back_populates="tareas")
+
+    def marcar_completada(self):
+        self.completada = True
+
+    def desmarcar_completada(self): 
+        self.completada = False
