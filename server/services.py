4ea70618e2836d7ac8f9cfdb43e9fdbb55cec4f8
Lucy <luu.anzoateguy@gmail.com>
2025-12-08 13:52:49 -0300

    Lógica en servicios de objetivos

--- server/services.py
+++ server/services.py
@@ -16,3 +16,27 @@ def obtener_usuario(session:Session, usuario_id: int):
     if not usuario:
         return {"error": "Usuario no encontrado"},  404
 
+def crear_objetivo(session, usuario_id, titulo):
+    existe = session.query(Objetivo).filter_by(usuario_id=usuario_id, titulo=titulo).first()
+    if existe:
+        return {"error": "El objetivo ya existe"}, 400
+    
+    objetivo = Objetivo(titulo=titulo, usuario_id=usuario_id)
+    session.add(objetivo)
+    session.commit()
+    return {"mensaje": "Objetivo creado con éxito", "id": objetivo.id}, 201
+
+def obtener_objetivos(session:Session, usuario_id:int):
+    objetivos = session.query(Objetivo).filter_by(usuario_id=usuario_id).all()
+    if not objetivos:
+        return {"error": "No hay objetivos creados"}, 404
+    
+    resultado = []
+    for obj in objetivos:
+        resultado.append({
+            "id": obj.id,
+            "titulo": obj.titulo,
+            "progreso": obj.progreso()
+        })
+
+    return {"objetivos": resultado}, 200
