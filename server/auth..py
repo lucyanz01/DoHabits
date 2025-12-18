a19379fe0d712b54764faaec8049da8f9b8e23ed
Lucy <luu.anzoateguy@gmail.com>
2025-12-18 11:28:14 -0300

    Creo función e importo blueprints y timezone

--- server/auth..py
+++ server/auth..py
@@ -1,8 +1,29 @@
 import jwt 
 import os
-from datetime import datetime, timedelta
-from flask import jsonify, request
+from datetime import datetime, timedelta, timezone
+from flask import jsonify, request, Blueprint, current_app
 from functools import wraps
 
 SECRET_KEY = os.getenv("SECRET_KEY")
 ALGORITHM = "HS256"
+
+if not SECRET_KEY:
+    raise RuntimeError("SECRET_KEY no definida")
+
+bp = Blueprint("auth", __name__)
+
+def crear_token(usuario_id):
+    payload = {
+        "sub": usuario_id,
+        "iat": datetime.now(timezone.utc),
+        "exp": datetime.now(timezone.utc) + timedelta(hours=24)
+    }
+
+    token = jwt.encode(
+        payload,
+        SECRET_KEY,
+        algorithm=ALGORITHM
+    )
+
+    return token
+
