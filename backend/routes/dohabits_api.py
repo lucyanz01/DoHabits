057dc94cffc696be1997c42c23bcfe0e629263b2
Lucy <luu.anzoateguy@gmail.com>
2025-12-18 13:01:22 -0300

    Cambio nombre al importar

--- backend/routes/dohabits_api.py
+++ backend/routes/dohabits_api.py
@@ -2,7 +2,7 @@ from flask import Flask, request, jsonify
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from models import Base, Usuario, Objetivo, Tarea
-from services import (
+from services.services import (
     crear_usuario,
     obtener_usuario,
     crear_objetivo,
