3818a945aedb01dc719734e7edf180e5fc688dbc
Lucy <luu.anzoateguy@gmail.com>
2025-12-23 14:06:17 -0300

    Añado dos rutas: marcar y desmarcar tarea

--- backend/routes/dohabits_api.py
+++ backend/routes/dohabits_api.py
@@ -1,5 +1,6 @@
 from flask import Blueprint, request, jsonify
 from config import session
+from models import Tarea, Objetivo
 from backend.routes.auth import token_required
 from backend.services.objetivos import crear_objetivo, obtener_objetivos
 from backend.services.tareas import crear_tarea, obtener_tareas
@@ -29,3 +30,39 @@ def manejar_tareas(obj_id):
     data = request.get_json()
     resultado, status = crear_tarea(session, data.get("descripcion"), user_id, obj_id)
     return jsonify(resultado), status
+
+@bp_api.route("/tareas/<int:tarea_id>/completar", methods=["PATCH"])
+@token_required
+def tarea_completada(tarea_id):
+    user_id = request.user_id
+    
+    tarea = session.query(Tarea).join(Objetivo).filter(
+        Tarea.id == tarea_id,
+        Objetivo.usuario_id == user_id
+    ).first()
+
+    if not tarea:
+        return jsonify({"error": "No encontrada"}), 404
+    
+    tarea.marcar_completada()
+    session.commit()
+
+    return jsonify({"mensaje": f"Tarea '{tarea.descripcion}' marcada como completada"}), 200
+
+@bp_api.route("/tareas/<int:tarea_id>/desmarcar", methods=["PATCH"])
+@token_required
+def desmarcar(tarea_id):
+    user_id = request.user_id
+
+    tarea = session.query(Tarea).join(Objetivo).filter(
+        Tarea.id == tarea_id, 
+        Objetivo.usuario_id == user_id
+    ).first()
+
+    if not tarea:
+        return jsonify({"error": "No encontrada"}), 404
+    
+    tarea.desmarcar_completada()
+    session.commit()
+
+    return jsonify({"mensaje": "Tarea desmarcada"}), 200
